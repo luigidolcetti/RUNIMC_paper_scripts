39c48c175896fa90580a561448c672353889f461
luigidolcetti <63606658+luigidolcetti@users.noreply.github.com>
2021-09-16 18:33:46 +0100

    Add files via upload

---
+++ BH_TEST3.1/REP/box_switch.R
@@ -0,0 +1,44 @@
+ent<-read.csv(
+  "C:/Users/luigi/Documents/BH_TEST/BH_TEST3.1/REP_1/RESULTS_FAMILY/entropy.csv"  
+)
+
+ent$Method<-factor(ent$Method,levels=c('BO','RU'))
+
+ggp<-ggplot2::ggplot()+ggplot2::geom_boxplot(mapping =ggplot2::aes(y=entropy,
+                                                                   x=density,
+                                                                   fill = Method),
+                                             position = 'dodge',
+                                             data = ent)+
+  ggplot2::scale_fill_manual(values = c('white','gray50'))+
+  ggplot2::ylim(c(0,1))+
+  ggplot2::theme_classic()
+
+ggplot2::ggsave(plot = ggp,
+                filename = "C:/Users/luigi/Documents/BH_TEST/BH_TEST3.1/REP_1/RESULTS_FAMILY/entropy_BS.eps",
+                device = 'eps',
+                width = 5,
+                height = 5,
+                dpi = 600)
+
+
+IOU<-read.csv(
+  "C:/Users/luigi/Documents/BH_TEST/BH_TEST3.1/REP_1/RESULTS_FAMILY/IOU.csv"  
+)
+
+IOU$Method<-factor(IOU$Method,levels=c('BO','RU'))
+
+ggp<-ggplot2::ggplot()+ggplot2::geom_boxplot(mapping =ggplot2::aes(y=IOU,
+                                                                   x=density,
+                                                                   fill = Method),
+                                             position = 'dodge',
+                                             data = IOU)+
+  ggplot2::scale_fill_manual(values = c('white','gray50'))+
+  ggplot2::ylim(c(0,1))+
+  ggplot2::theme_classic()
+
+ggplot2::ggsave(plot = ggp,
+                filename = "C:/Users/luigi/Documents/BH_TEST/BH_TEST3.1/REP_1/RESULTS_FAMILY/IOU_BS.eps",
+                device = 'eps',
+                width = 5,
+                height = 5,
+                dpi = 600)
